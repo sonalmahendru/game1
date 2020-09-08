06adab2172d720096ee3f2dda4230a3e64e0a19f
sonalmahendru <52343997+sonalmahendru@users.noreply.github.com>
2020-09-08 07:06:14 +0530

    Update sketch.js
    Added some variables

--- sketch.js
+++ sketch.js
@@ -3,7 +3,8 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1, box2;
+var box1, pig1, box2,log3, box4, box5, log4, log5;
+var bird;
 var backgroundImg, platform;
 
 function preload() {
